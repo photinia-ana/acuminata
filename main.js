f3bc4ba9fba57acdb4f2696195a6e1d062042aee
Roy <roooyhe@163.com>
2026-05-11 23:33:54 +0800

    change

--- main.js
+++ main.js
@@ -743,7 +743,20 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, "ui", "index.html"));
+  // 判断是否为开发环境
+  const isDev =
+    process.env.NODE_ENV === "development" || process.argv.includes("--dev");
+
+  if (isDev) {
+    // 【开发环境】加载 Vite 的热更新本地服务
+    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.webContents.openDevTools();
+  } else {
+    // 【生产环境】加载 Vite 打包后的 dist 目录文件
+    mainWindow.loadFile(
+      path.join(__dirname, "src-renderer", "dist", "index.html"),
+    );
+  }
 
   mainWindow.once("ready-to-show", () => {
     mainWindow.show();
